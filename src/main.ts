b2cca9ea5a43698250948501a29571cc18f4f563
iamKhang <iamhoangkhang@icloud.com>
2025-02-15 08:02:26 +0700

    Thêm Swagger cho dự án

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Edu Force')
+    .setDescription('API cung cấp các chức năng cho việc quản lý ngân hàng câu hỏi, quản lý bài kiểm tra và điểm số, ...')
+    .setVersion('1.0')
+    .addTag('edu-forge-assessment')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
